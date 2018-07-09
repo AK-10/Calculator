40bc0d5e989daf496b26078593c217ada17907ff
Atsushi KONISHI <crossover.ak@outlook.jp>
2018-07-09 11:35:58 +0900

    made AST

--- calculator.hs
+++ calculator.hs
@@ -1,15 +1,18 @@
 import Data.Char
+-- import Data.Monad
 
+-- 値
 data Value = INT Integer | REAL Double deriving(Show, Eq)
 
+-- トークン
 data Token = 
-      Number Value
-    | Add | Sub | Mul | Div
-    | Lpar | Rpar
-    | Semic
-    | EOF
-    | Others Char
-    deriving(Show, Eq)
+    Number Value  -- ex. (INT 10), (REAL 103.4)
+  | Add | Sub | Mul | Div
+  | Lpar | Rpar
+  | Semic
+  | EOF
+  | Others Char
+  deriving(Show, Eq)
 
 type Lexer = (Token, String) -- 文字列の先頭からTokenを抽出したものと残りのStringのペア
 
@@ -33,6 +36,7 @@ getToken (x:xs)
       ';' -> (Semic, xs)
       _   -> (Others x, xs)
 
+-- 字句解析
 lexer :: String -> ([Token], String)
 lexer xs =
   let (t, ys) = getToken xs
@@ -42,3 +46,68 @@ lexer xs =
     _     -> let (ts, zs) = lexer ys
               in (t:ts, zs)
 
+
+-- 構文解析              
+-- 構文木
+data Expr = Num Value
+  | Op1 (Value -> Value) Expr -- 単項演算 ex. -1
+  | Op2 (Value -> Value -> Value) Expr Expr　-- 二項演算 ex. (1 * 3) + (1 - (3 / 1))
+
+type Calc a = Either String a -- Monadわからない
+
+-- エラー処理
+calcError :: String -> Calc a
+calcError s = Left s
+
+-- 数式の計算
+-- 式がexpr, 項がterm, 因子がfactor 正常に解析ができたときは Right (Expr, [Token]), 式に間違いがあれば Left Stringを返す
+
+-- リストの先頭トークンがNumberであれば(Num x, xs)を返す. return でEitherモナドに包む
+-- '('であればexprを')'が来るで再帰処理して式eを求めEitherモナドに包んで(e, ys)を返す. ')'がなければcalcErrorでLeft Stringを返す
+-- トークンがSubであればexprを再帰呼び出しして値をOp1にセットして返す. negは単項演算子 - を処理する関数
+factor :: [Token] -> Calc (Expr, [Token])
+factor (Number x : xs) = return (Num x, xs) -- 先頭がNumberだったときの処理
+factor (Lpar x : xs) = expr xs >>= \(e, y:ys) ->  -- >>= は連結を表す(おそらくexpr xs の返り値をラムダ計算の引数に入力するという意味だと思う)
+  case y of
+    Rpar -> return (e, ys)
+    _    -> calcError "')' expected"
+factor (Sub:xs) = expr xs >>= \(e, ys) -> return (Op1 neg e, ys) 
+factor (Add:xs) = expr xs
+factor (EOF:xs) = calcError "End of File"
+factor (x:_)    = calcError ("unexpected Token" ++ show x)
+
+-- 最初にfactorを呼び出し, 因子の値を局所変数term_subに渡す
+-- トークンがMul, Divの場合factorを呼び出して次の因子を求めOp2に格納してterm_subを再帰呼び出し.
+-- トークンがMul, Div以外の場合zsをEitherモナドに包んで返す
+term :: [Token] -> Calc (Expr, [Token])
+term xs = factor xs >>= term_sub
+  where
+    opList = [(Mul, mul), (Div, div')]
+    term_sub zs@(e, y:ys) = 
+      case lookup y opList of
+        Nothing -> return zs
+        Just op -> factor ys >>= \(e', ys') -> term_sub (Op2 op e e', ys')
+
+
+-- term のAdd, Sub版
+expr :: [Token] -> Calc (Expr, [Token])
+expr xs = term xs >>= expr_sub
+  where
+    opList = [(Add, add), (Sub, sub)]
+    expr_sub zs@(e, y:ys) = 
+      case lookup y opList of
+        Nothing -> return zs
+        Just op -> term ys >>= \(e', ys') -> expr_sub (Op2 op e e', ys')
+
+-- exprを呼び出す関数
+expression ::[Token] -> Calc (Expr, [Token])
+expression xs = expr xs >>= \(e, y:ys) ->
+  case y of
+    Semic -> return (e, ys)
+    _     -> calcError "expression error"
+
+
+-- 処理の流れ
+-- 1. lexerにて文字列で与えられた式を字句解析, Tokenのリストにする
+-- 2. expressionにてlexerから受け取ったTokenのリストをASTに落とし込む
+--     exprを再帰呼び出しして
